2557cfadf757f3cdce09b41dd61ef45d9b3f46d7
Vrits <saidhalim687@gmail.com>
2024-02-26 15:16:44 +0800

    fix: added min-width

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   };
   return (
     <>
-      <main className="container lg:max-w-4xl mx-auto px-4 text-slate-800 ">
+      <main className="container lg:max-w-4xl mx-auto px-4 text-slate-800 min-w-[640px]">
         <div className="mx-auto flex flex-col w-max">
           <h1 className="font-bold text-4xl mt-8 ubuntu- py-2">
             Rekening Pedia
