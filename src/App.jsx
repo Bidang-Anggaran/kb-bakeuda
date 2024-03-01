57410e7b2fc8ed9bb34393345ef47dd68f808d1d
Vrits <saidhalim687@gmail.com>
2024-03-02 07:35:10 +0800

    fix:resored bg, removed drop shadow

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <BelanjaProvider>
-        <main className="container lg:max-w-4xl xl:max-w-6xl 2xl:max-w-screen-2xl mx-auto text-slate-800 min-w-[768px] drop-shadow-lg">
+        <main className="container lg:max-w-4xl xl:max-w-6xl 2xl:max-w-screen-2xl mx-auto text-slate-800 min-w-[768px] ">
         <div className="mx-auto flex flex-col w-max">
           <h1 className="font-bold text-4xl mt-8 ubuntu-bold drop-shadow-lg text-sky-900 py-2">
             REKENING PEDIA
